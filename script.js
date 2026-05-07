6b5afee339eaa0ceeed427f9ca667c4d11d01b6d
hudagamal <hudagamal052@gmail.com>
2026-05-07 18:08:16 +0300

    add the first version of js project

---
+++ script.js
@@ -0,0 +1,8 @@
+//Declaration and Assigning
+//Reassigning
+//Redaclaration !!!!!!
+let name = "Huda 'Gamal' Ahmed";
+let age = 24;
+
+document.getElementById("output1").innerHTML = name;
+document.getElementById("output2").innerHTML = age;
